6f8f10afb490d846980c9f7be5b94c06b44afc22
Henry Song <henry.song671@gmail.com>
2021-07-07 08:35:53 +1000

    update MatLab DFE

--- Source Code/Compartment_Model_DiffEq.m
+++ Source Code/Compartment_Model_DiffEq.m
@@ -4,9 +4,9 @@ mu2 = 0.22; % natural death rate of parrotfish
 q = 0.47; % intrinsic growth rate for parrotfish
 alpha = 1; % %maximum grazing intensity
 sigma = 0.01; % rate that parrotfish bite corals
-r = 0.1; % rate that coral recruit to overgrow algal turfs
+r = 0.5; % rate that coral recruit to overgrow algal turfs
 gamma = 0.8; %rate that macroalgae spread vegetative over algal turfs
-beta = 0.9; % carrying capacity
+beta = 1; % carrying capacity
 
 a0 = 0.99; % rate that coral is overgrown by macroalgae
 h = 0.1; %<----CONTROL VARIABLE FOR GAME THEORY
@@ -19,7 +19,7 @@ a = @(t) abs((a0*(9*sin(pi*t)+1))/(10));
 
 % set up SoDEx
 % dC/dt = rTC + sigmaPC - C(aM + mu1)
-% dP/dt = qP(1-P/betaC) + kapaP - (h + mu)P #remove kapaP, so  = qP(1-P/betaC) - (h + mu)P
+% dP/dt = qP(1-P/betaC) + kapaP - (h + mu2)P #remove kapaP, so  = qP(1-P/betaC) - (h + mu)P
 % dT/dt = mu1C + (g(P)M)/(M + T) - (rC + gammaM)T
 % dM/dt = aMC + gammaMT - (g(P)M)/(M + T)
 %
@@ -33,11 +33,11 @@ f = @(t,y) [r*y(3)*y(1) + sigma*y(2)*y(1) - y(1)*(a(t)*y(4) + mu1),
 C = 1/3;
 P = 0.75;
 T = 1/3;
-M = 1/3;
+M = 0;
         
   
 % solve with ODE 45
-[t,ya] = ode45(f, [0 10], [C, P, T, M, 1]);
+[t,ya] = ode45(f, [0 5], [C, P, T, M, 1]);
 
 % graph
 figure

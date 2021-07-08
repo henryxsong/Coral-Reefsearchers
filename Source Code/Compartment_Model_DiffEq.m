242629264691da5c8b4a399f3589fd37664562ac
Henry Song <henry.song671@gmail.com>
2021-07-08 20:09:06 +1000

    Update Compartment_Model_DiffEq.m

--- Source Code/Compartment_Model_DiffEq.m
+++ Source Code/Compartment_Model_DiffEq.m
@@ -14,21 +14,14 @@ h = 0.1; %<----CONTROL VARIABLE FOR GAME THEORY
 %grazing intensity 'g'
 g = @(P) (alpha*P)/beta;
 
-<<<<<<< HEAD
 %sin function of 
 a = @(t) abs((a0*(9*sin(pi*t)+1))/(10));
 
-% set up SoDEx
-% dC/dt = rTC + sigmaPC - C(aM + mu1)
-% dP/dt = qP(1-P/betaC) + kapaP - (h + mu2)P #remove kapaP, so  = qP(1-P/betaC) - (h + mu)P
-% dT/dt = mu1C + (g(P)M)/(M + T) - (rC + gammaM)T
-=======
 % set up DFE
 % dC/dt = rTC + sigmaPC - C(aM + d) #ignore sigmaPC for now
 % dP/dt = qP(1-P/betaC) + kapaP - (h + mu)P 
 % #remove kapaP, so = qP(1-P/betaC) - (h + mu)P
 % dT/dt = dC + (g(P)M)/(M + T) - (rC + gammaM)T
->>>>>>> origin/overleaf-2021-07-07-1238
 % dM/dt = aMC + gammaMT - (g(P)M)/(M + T)
 %
 % C = y(1), P = y(2), T = y(3), M = y(4)
@@ -39,9 +32,9 @@ f = @(t,y) [r*y(3)*y(1) + sigma*y(2)*y(1) - y(1)*(a(t)*y(4) + mu1),
             y(1)+y(3)+y(4)];
 
 C = 1/3;
-P = 0.75;
+P = 3/4;
 T = 1/3;
-M = 0;
+M = 1/3;
         
   
 % solve with ODE 45

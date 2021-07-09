2bae1622a8abbbc2a2f3b5149cf5ae5bfad4488b
Henry Song <henry.song671@gmail.com>
2021-07-09 20:10:22 +1000

    push

--- Source Code/Compartment_Model_DiffEq.m
+++ Source Code/Compartment_Model_DiffEq.m
@@ -37,50 +37,60 @@ M = 1/2;
 %             mu1*y(1) +  (g(y(2))*y(4))/(y(4)+y(3)) - (r*y(1) + gamma*y(4))*y(3),
 %             a(t) * y(4)*y(1) + gamma*y(4)*y(3) - (g(y(2))*y(4))/(y(4)+y(3)),
 %             y(1)+y(3)+y(4)];
-            
-[t,ya] = ode45(f, [0 5], [C, P, T, M, 1]);
+%             
+% [t,ya] = ode45(f, [0 5], [C, P, T, M, 1]);
+% 
+% figure
+% hold on
+% plot(t, ya(:,1), '+-.', 'Color', '#FFC996', 'Linewidth', 2.5)
+% plot(t, ya(:,2), 'x-.', 'Color', '#4974A5', 'Linewidth', 2.5)
+% plot(t, ya(:,3), 'o-.', 'Color', '#BDD2B6', 'Linewidth', 2.5)
+% plot(t, ya(:,4), '*-.', 'Color', '#CF0000', 'Linewidth', 2.5)
+% %legend('Coral (C)', 'Algal Turf (T)', 'Macroalgae (M)')
+% set(gca, 'FontSize',18);
+% ylim([0 1]);
+% legend('Coral (C)', 'Parrotfish (P)', 'Algal Turf (T)', 'Macroalgae (M)')
+% %text(0.25,0.05,txt, 'FontSize', 18);
+% xlabel('Time (Year)')
+% ylabel('Proportion of Population')
 
-figure
-hold on
-plot(t, ya(:,1), '+-.', 'Color', '#FFC996', 'Linewidth', 2.5)
-plot(t, ya(:,2), 'x-.', 'Color', '#4974A5', 'Linewidth', 2.5)
-plot(t, ya(:,3), 'o-.', 'Color', '#BDD2B6', 'Linewidth', 2.5)
-plot(t, ya(:,4), '*-.', 'Color', '#CF0000', 'Linewidth', 2.5)
-%legend('Coral (C)', 'Algal Turf (T)', 'Macroalgae (M)')
-set(gca, 'FontSize',18);
-ylim([0 1]);
-legend('Coral (C)', 'Parrotfish (P)', 'Algal Turf (T)', 'Macroalgae (M)')
-%text(0.25,0.05,txt, 'FontSize', 18);
-xlabel('Time (Year)')
-ylabel('Proportion of Population')
-  
-% for i = 1:length(t)
-%     a0 = i/length(t);
-%     a = @(t) abs((a0*(9*sin(pi*t)+1))/(10));
-%     f = @(t,y) [r*y(3)*y(1) + sigma*y(2)*y(1) - y(1)*(a(t)*y(4) + mu1),
-%             q*y(2)*(1-(y(2)/(beta*y(1)))) - (h+mu2)*y(2), 
-%             mu1*y(1) + (g(y(2))*y(4))/(y(4)+y(3)) - (r*y(1) + gamma*y(4))*y(3),
-%             a(t) * y(4)*y(1) + gamma*y(4)*y(3) - (g(y(2))*y(4))/(y(4)+y(3)),
-%             y(1)+y(3)+y(4)];
-%     
-%     [t,ya] = ode45(f, [0 5], [C, P, T, M, 1]);
-%     
-%     txt = ['a_0 = ' num2str(a0)];
-%     figure
-%     hold on
-%     plot(t, ya(:,1), '+-.', 'Color', '#FFC996', 'Linewidth', 2.5)
-%     plot(t, ya(:,2), 'x-.', 'Color', '#4974A5', 'Linewidth', 2.5)
-%     plot(t, ya(:,3), 'o-.', 'Color', '#BDD2B6', 'Linewidth', 2.5)
-%     plot(t, ya(:,4), '*-.', 'Color', '#CF0000', 'Linewidth', 2.5)
-%     %legend('Coral (C)', 'Algal Turf (T)', 'Macroalgae (M)')
-%     set(gca, 'FontSize',18);
-%     ylim([0 1]);
-%     legend('Coral (C)', 'Parrotfish (P)', 'Algal Turf (T)', 'Macroalgae (M)')
-%     text(0.25,0.05,txt, 'FontSize', 18);
-%     xlabel('Time (Year)')
-%     ylabel('Proportion of Population')
-% end
-% solve with ODE 45
+% For-loop for animating parameter change
+for i = 1:length(t)
+    a0 = i/length(t);
+    a = @(t) abs((a0*(9*sin(pi*t)+1))/(10));
+    
+    % System of Differential Equations
+    f = @(t,y) [r*y(3)*y(1) + sigma*y(2)*y(1) - y(1)*(a(t)*y(4) + mu1),
+            q*y(2)*(1-(y(2)/(beta*y(1)))) - (h+mu2)*y(2), 
+            mu1*y(1) + (g(y(2))*y(4))/(y(4)+y(3)) - (r*y(1) + gamma*y(4))*y(3),
+            a(t) * y(4)*y(1) + gamma*y(4)*y(3) - (g(y(2))*y(4))/(y(4)+y(3)),
+            y(1)+y(3)+y(4)];
+    
+    % Solve using ODE45
+    [t,ya] = ode45(f, [0 5], [C, P, T, M, 1]);
+    
+    % Plot
+    txt = ['a_0 = ' num2str(a0)]; % shows value of param value at current iteration
+    
+    fig = figure;
+    hold on
+    plot(t, ya(:,1), '+-.', 'Color', '#FFC996', 'Linewidth', 2.5)
+    plot(t, ya(:,2), 'x-.', 'Color', '#4974A5', 'Linewidth', 2.5)
+    plot(t, ya(:,3), 'o-.', 'Color', '#BDD2B6', 'Linewidth', 2.5)
+    plot(t, ya(:,4), '*-.', 'Color', '#CF0000', 'Linewidth', 2.5)
+    
+    set(gca, 'FontSize',18); % sets axis & legend font size to 18
+    ylim([0 1]); % sets y-axis limit to always be 0-1
+    legend('Coral (C)', 'Parrotfish (P)', 'Algal Turf (T)', 'Macroalgae (M)')
+    text(0.25,0.05,txt, 'FontSize', 18); % displays text on plot
+    
+    xlabel('Time (Year)')
+    ylabel('Proportion of Population')
+    
+    % automatically save figure into root directory (where this .m file is
+    % stored)
+    filename = append('Frame-', num2str(i)); %file name of current iteration
+    saveas(fig, filename, 'png'); %save figure as .png
+end
 
 
-% graph

379c30ec3ea95dd3333e75fa9cf4b47a28aa82a2
Henry Song <henry.song671@gmail.com>
2021-07-12 22:24:47 +1000

    Update Disease_Free_Equilibrium.m

--- Source Code/Disease_Free_Equilibrium.m
+++ Source Code/Disease_Free_Equilibrium.m
@@ -2,53 +2,53 @@ syms C P T M mu1 mu2 q omega sigma beta r h phi g a t
 % g = g(P), a = a(t)
 
 %grazing intensity 'g'
-%g = @(P) (omega*P)/beta;
+g = @(P) (omega*P)/beta;
 
 %sin function of 
 %a = @(t) abs((0.9*(9*sin(pi*t)+1))/(10));
 
 %-----------------------------------------------------
 %solving DFE
-% dMdt = a*C*M + phi*T*M - (g*M)/(M+T);
-% %M_0 = solve(dMdt == 0, M); % solves for M when dMdt = 0
-% %M0 = subs(M0, [C,P,T], [0,0,0]);
-% M_0 = 0;
-% 
-% dTdt = mu1*C + (g(P)*M_0)/(M_0+T) - T*(r*C + phi*M_0);
-% T_0 = solve(dTdt == 0, T);
-% 
-% %dCdt = r*T_0*C + sigma*P_0*C - (a*M_0 + mu1)*C;
-% %C_0 = solve(dCdt == 0, C);
-% C_0 = 1 - T_0;
-% 
-% dPdt = q*P*(1-(P/(beta*C))) - P*(h+mu2);
-% P_0 = solve(dPdt == 0, P);
-% %P_0 = 0;
+dMdt = a*C*M + phi*T*M - (g*M)/(M+T);
+%M_0 = solve(dMdt == 0, M); % solves for M when dMdt = 0
+%M0 = subs(M0, [C,P,T], [0,0,0]);
+M_0 = 0;
+
+dTdt = mu1*C + (g(P)*M_0)/(M_0+T) - T*(r*C + phi*M_0);
+T_0 = solve(dTdt == 0, T);
+
+%dCdt = r*T_0*C + sigma*P_0*C - (a*M_0 + mu1)*C;
+%C_0 = solve(dCdt == 0, C);
+C_0 = 1 - T_0;
+
+dPdt = q*P*(1-(P/(beta*C))) - P*(h+mu2);
+P_0 = solve(dPdt == 0, P);
+%P_0 = 0;
 %------------------------------------------------------
 
-% %------------------------------------------------------
-% % % Solving R0
-% 
-% %script F
-% sF = [a*C*M + phi*T*M];
-% F = jacobian(sF, [M]); % jacobian matrix
-% F = subs(F, T, T_0); 
-% %F = subs(F, M, 0);
-% %F = subs(F, C, C_0);
-% 
-% %script V
-% syms G;
-% sV = [(G*M)/(M+T)];
-% V = jacobian(sV, [M]);
-% V = subs(V, M, M_0);
-% 
-% %eigenvalues of F*V^-1
-% eigens = eig(F * inv(V));
-% 
-% % basic reproduction number
-% R0 = eigens(1);
-% R0 = subs(R0, C, C_0);
-% R0 = subs(R0, T, T_0);
+%------------------------------------------------------
+% % Solving R0
+
+%script F
+sF = [a*C*M + phi*T*M];
+F = jacobian(sF, [M]); % jacobian matrix
+F = subs(F, T, T_0); 
+%F = subs(F, M, 0);
+%F = subs(F, C, C_0);
+
+%script V
+syms G;
+sV = [(g(P)*M)/(M+T)];
+V = jacobian(sV, [M]);
+V = subs(V, M, M_0);
+
+%eigenvalues of F*V^-1
+eigens = eig(F * inv(V));
+
+% basic reproduction number
+R0 = eigens(1);
+R0 = subs(R0, C, C_0);
+R0 = subs(R0, T, T_0);
 %------------------------------------------------------
 
 
@@ -77,32 +77,32 @@ syms C P T M mu1 mu2 q omega sigma beta r h phi g a t
 % C_E = solve(dCdt == 0, C, ReturnConditions = true);
 % %------------------------------------------------------
 
-T_E = (mu1 + a*M)/r;
-
-C_E = 1 - (T+M);
-C_E = subs(C_E, T, T_E);
-
-P_E = (beta*C*(q-(h+mu2)))/q;
-P_E = subs(P_E, C, C_E);
-%testP = (beta*(1-((mu1+a*M)/r) + M)*(q-(h+mu2)))/(q);
-
-M_E = (omega*P)/(beta*(a*C+phi*T))-T;
-M_E = subs(M_E, P, P_E);
-M_E = subs(M_E, C, C_E);
-M_E = subs(M_E, T, T_E);
-%testM = (omega*(beta*(1-(mu1+a*M)/r + M)*(q-(h+mu2))))/(q*beta*(a*(1-((mu1+a*M)/r + M))) + phi*((mu1+a*M)/r)) - (mu1+a*M)/r;
-ansM = solve(M_E == 0, M);
-l = latex(ansM);
-
-% My quadratic equation
-a_quad = a^2*q*phi - a^2*q*r - a^3*q;
-b_quad = a^2*q*r - a*mu1*q*r - 2*a^2*mu1*q + 2*a*mu1*q*phi + omega*a*q - omega*a*h - omega*a*mu2 + omega*r*q - omega*r*h - omega*r*mu2;
-c_quad = a*mu1*q*r - a*mu1^2*q + mu1^2*q*phi - omega*r*q + omega*r*h + omega*r*mu2 + omega*mu1*q - omega*mu1*h - omega*mu1*mu2;
-
-% Dr. Choi's quadratic equation
-% a_quad = a*q*(a+r-phi)*(a+r)^2;
-% b_quad = (a+r)*(2*a*q*(a+r-phi)*(r-mu1) - r*(omega*(a+r)*(q-h-mu2) - a*q*(a+r) + g*mu1*phi));
-% c_quad = a*q*(a+r-phi)*(r-mu1)^2 + r*(omega*(a+r)*(g-h-mu2) - a*q*(a+r) + q*mu1*phi)*(r-mu1) - q*phi*r^2*(mu1+a);
-% quad_quad = [((-b_quad)+((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad); ((-b_quad)-((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad)];
-
-quad_ans = solve(M^2*a_quad + M*b_quad + c_quad == 0, M);
+% T_E = (mu1 + a*M)/r;
+% 
+% C_E = 1 - (T+M);
+% C_E = subs(C_E, T, T_E);
+% 
+% P_E = (beta*C*(q-(h+mu2)))/q;
+% P_E = subs(P_E, C, C_E);
+% %testP = (beta*(1-((mu1+a*M)/r) + M)*(q-(h+mu2)))/(q);
+% 
+% M_E = (omega*P)/(beta*(a*C+phi*T))-T;
+% M_E = subs(M_E, P, P_E);
+% M_E = subs(M_E, C, C_E);
+% M_E = subs(M_E, T, T_E);
+% %testM = (omega*(beta*(1-(mu1+a*M)/r + M)*(q-(h+mu2))))/(q*beta*(a*(1-((mu1+a*M)/r + M))) + phi*((mu1+a*M)/r)) - (mu1+a*M)/r;
+% ansM = solve(M_E == 0, M);
+% l = latex(ansM);
+% 
+% % My quadratic equation
+% a_quad = a^2*q*phi - a^2*q*r - a^3*q;
+% b_quad = a^2*q*r - a*mu1*q*r - 2*a^2*mu1*q + 2*a*mu1*q*phi + omega*a*q - omega*a*h - omega*a*mu2 + omega*r*q - omega*r*h - omega*r*mu2;
+% c_quad = a*mu1*q*r - a*mu1^2*q + mu1^2*q*phi - omega*r*q + omega*r*h + omega*r*mu2 + omega*mu1*q - omega*mu1*h - omega*mu1*mu2;
+% 
+% % Dr. Choi's quadratic equation
+% % a_quad = a*q*(a+r-phi)*(a+r)^2;
+% % b_quad = (a+r)*(2*a*q*(a+r-phi)*(r-mu1) - r*(omega*(a+r)*(q-h-mu2) - a*q*(a+r) + g*mu1*phi));
+% % c_quad = a*q*(a+r-phi)*(r-mu1)^2 + r*(omega*(a+r)*(g-h-mu2) - a*q*(a+r) + q*mu1*phi)*(r-mu1) - q*phi*r^2*(mu1+a);
+% % quad_quad = [((-b_quad)+((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad); ((-b_quad)-((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad)];
+% 
+% quad_ans = solve(M^2*a_quad + M*b_quad + c_quad == 0, M);

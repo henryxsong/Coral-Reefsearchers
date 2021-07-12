82e6398444471147a76a2d2a35fb40f6479c2d54
Henry Song <henry.song671@gmail.com>
2021-07-12 20:50:42 +1000

    updated matlab
    - changed alpha to omega
    - changed gamma to phi
    - new endemic equilibrium calculations

--- Source Code/Compartment_Model_DiffEq.m
+++ Source Code/Compartment_Model_DiffEq.m
@@ -2,26 +2,29 @@
 mu1 = 0.15; % mortality rate of coral reefs
 mu2 = 0.22; % natural death rate of parrotfish
 q = 0.47; % intrinsic growth rate for parrotfish
-alpha = 1; % %maximum grazing intensity
+omega = 1; % %maximum grazing intensity
 sigma = 0.01; % rate that parrotfish bite corals
 r = 0.5; % rate that coral recruit to overgrow algal turfs
-gamma = 0.8; %rate that macroalgae spread vegetative over algal turfs
+phi = 0.8; %rate that macroalgae spread vegetative over algal turfs
 beta = 1; % carrying capacity
 
 a0 = 0.99; % rate that coral is overgrown by macroalgae
 h = 0.1; %<----CONTROL VARIABLE FOR GAME THEORY
 
 %grazing intensity 'g'
-g = @(P) (alpha*P)/beta;
+g = @(P) (omega*P)/beta;
 
 %sin function of 
 a = @(t) abs((a0*(9*sin(pi*t)+1))/(10));
+
+t = 0;
 
 % Compartment Initial Conditions
 C = 1/4;
 P = 3/4;
 T = 1/4;
 M = 1/2;
+
 
 
 % set up DFE
@@ -55,42 +58,42 @@ M = 1/2;
 % ylabel('Proportion of Population')
 
 % For-loop for animating parameter change
-for i = 1:length(t)
-    a0 = i/length(t);
-    a = @(t) abs((a0*(9*sin(pi*t)+1))/(10));
-    
-    % System of Differential Equations
-    f = @(t,y) [r*y(3)*y(1) + sigma*y(2)*y(1) - y(1)*(a(t)*y(4) + mu1),
-            q*y(2)*(1-(y(2)/(beta*y(1)))) - (h+mu2)*y(2), 
-            mu1*y(1) + (g(y(2))*y(4))/(y(4)+y(3)) - (r*y(1) + gamma*y(4))*y(3),
-            a(t) * y(4)*y(1) + gamma*y(4)*y(3) - (g(y(2))*y(4))/(y(4)+y(3)),
-            y(1)+y(3)+y(4)];
-    
-    % Solve using ODE45
-    [t,ya] = ode45(f, [0 5], [C, P, T, M, 1]);
-    
-    % Plot
-    txt = ['a_0 = ' num2str(a0)]; % shows value of param value at current iteration
-    
-    fig = figure;
-    hold on
-    plot(t, ya(:,1), '+-.', 'Color', '#FFC996', 'Linewidth', 2.5)
-    plot(t, ya(:,2), 'x-.', 'Color', '#4974A5', 'Linewidth', 2.5)
-    plot(t, ya(:,3), 'o-.', 'Color', '#BDD2B6', 'Linewidth', 2.5)
-    plot(t, ya(:,4), '*-.', 'Color', '#CF0000', 'Linewidth', 2.5)
-    
-    set(gca, 'FontSize',18); % sets axis & legend font size to 18
-    ylim([0 1]); % sets y-axis limit to always be 0-1
-    legend('Coral (C)', 'Parrotfish (P)', 'Algal Turf (T)', 'Macroalgae (M)')
-    text(0.25,0.05,txt, 'FontSize', 18); % displays text on plot
-    
-    xlabel('Time (Year)')
-    ylabel('Proportion of Population')
-    
-    % automatically save figure into root directory (where this .m file is
-    % stored)
-    filename = append('Frame-', num2str(i)); %file name of current iteration
-    saveas(fig, filename, 'png'); %save figure as .png
-end
+% for i = 1:length(t)
+%     a0 = i/length(t);
+%     a = @(t) abs((a0*(9*sin(pi*t)+1))/(10));
+%     
+%     % System of Differential Equations
+%     f = @(t,y) [r*y(3)*y(1) + sigma*y(2)*y(1) - y(1)*(a(t)*y(4) + mu1),
+%             q*y(2)*(1-(y(2)/(beta*y(1)))) - (h+mu2)*y(2), 
+%             mu1*y(1) + (g(y(2))*y(4))/(y(4)+y(3)) - (r*y(1) + phi*y(4))*y(3),
+%             a(t) * y(4)*y(1) + phi*y(4)*y(3) - (g(y(2))*y(4))/(y(4)+y(3)),
+%             y(1)+y(3)+y(4)];
+%     
+%     % Solve using ODE45
+%     [t,ya] = ode45(f, [0 5], [C, P, T, M, 1]);
+%     
+%     % Plot
+%     txt = ['a_0 = ' num2str(a0)]; % shows value of param value at current iteration
+%     
+%     fig = figure;
+%     hold on
+%     plot(t, ya(:,1), '+-.', 'Color', '#FFC996', 'Linewidth', 2.5)
+%     plot(t, ya(:,2), 'x-.', 'Color', '#4974A5', 'Linewidth', 2.5)
+%     plot(t, ya(:,3), 'o-.', 'Color', '#BDD2B6', 'Linewidth', 2.5)
+%     plot(t, ya(:,4), '*-.', 'Color', '#CF0000', 'Linewidth', 2.5)
+%     
+%     set(gca, 'FontSize',18); % sets axis & legend font size to 18
+%     ylim([0 1]); % sets y-axis limit to always be 0-1
+%     legend('Coral (C)', 'Parrotfish (P)', 'Algal Turf (T)', 'Macroalgae (M)')
+%     text(0.25,0.05,txt, 'FontSize', 18); % displays text on plot
+%     
+%     xlabel('Time (Year)')
+%     ylabel('Proportion of Population')
+%     
+%     % automatically save figure into root directory (where this .m file is
+%     % stored)
+%     filename = append('Frame-', num2str(i)); %file name of current iteration
+%     saveas(fig, filename, 'png'); %save figure as .png
+% end
 
 
--- Source Code/Disease_Free_Equilibrium.m
+++ Source Code/Disease_Free_Equilibrium.m
@@ -1,20 +1,20 @@
-syms C P T M mu1 mu2 q alpha sigma r h gamma beta g a t
+syms C P T M mu1 mu2 q omega sigma beta r h phi g a t
 % g = g(P), a = a(t)
 
 %grazing intensity 'g'
-g = @(P) (alpha*P)/beta;
+%g = @(P) (omega*P)/beta;
 
 %sin function of 
 %a = @(t) abs((0.9*(9*sin(pi*t)+1))/(10));
 
 %-----------------------------------------------------
 %solving DFE
-% dMdt = a*C*M + gamma*T*M - (g*M)/(M+T);
+% dMdt = a*C*M + phi*T*M - (g*M)/(M+T);
 % %M_0 = solve(dMdt == 0, M); % solves for M when dMdt = 0
 % %M0 = subs(M0, [C,P,T], [0,0,0]);
 % M_0 = 0;
 % 
-% dTdt = mu1*C + (g(P)*M_0)/(M_0+T) - T*(r*C + gamma*M_0);
+% dTdt = mu1*C + (g(P)*M_0)/(M_0+T) - T*(r*C + phi*M_0);
 % T_0 = solve(dTdt == 0, T);
 % 
 % %dCdt = r*T_0*C + sigma*P_0*C - (a*M_0 + mu1)*C;
@@ -30,7 +30,7 @@ g = @(P) (alpha*P)/beta;
 % % % Solving R0
 % 
 % %script F
-% sF = [a*C*M + gamma*T*M];
+% sF = [a*C*M + phi*T*M];
 % F = jacobian(sF, [M]); % jacobian matrix
 % F = subs(F, T, T_0); 
 % %F = subs(F, M, 0);
@@ -57,10 +57,10 @@ g = @(P) (alpha*P)/beta;
 % dPdt = q*P*(1-(P/(beta*C))) - P*(h*mu2);
 % P_E = solve(dPdt == 0, P);
 % 
-% dMdt = a*C*M + gamma*T*M - (g*M)/(M+T);
+% dMdt = a*C*M + phi*T*M - (g*M)/(M+T);
 % M_E = solve(dMdt == 0, M);
 % 
-% dTdt = mu1*C + (g*M)/(M+T) - T*(r*C + gamma*M);
+% dTdt = mu1*C + (g*M)/(M+T) - T*(r*C + phi*M);
 % T_E = solve(dTdt == 0, T);
 % 
 % P_E = P_E(2);
@@ -86,10 +86,23 @@ P_E = (beta*C*(q-(h+mu2)))/q;
 P_E = subs(P_E, C, C_E);
 %testP = (beta*(1-((mu1+a*M)/r) + M)*(q-(h+mu2)))/(q);
 
-M_E = (alpha*P)/(beta*(a*C+gamma*T))-T;
+M_E = (omega*P)/(beta*(a*C+phi*T))-T - M_E;
 M_E = subs(M_E, P, P_E);
 M_E = subs(M_E, C, C_E);
 M_E = subs(M_E, T, T_E);
-%testM = (alpha*(beta*(1-(mu1+a*M)/r + M)*(q-(h+mu2))))/(q*beta*(a*(1-((mu1+a*M)/r + M))) + gamma*((mu1+a*M)/r)) - (mu1+a*M)/r;
+%testM = (omega*(beta*(1-(mu1+a*M)/r + M)*(q-(h+mu2))))/(q*beta*(a*(1-((mu1+a*M)/r + M))) + phi*((mu1+a*M)/r)) - (mu1+a*M)/r;
 ansM = solve(M_E == 0, M);
-l = latex(simplify(ansM(2)));
+l = latex(ansM);
+
+% My quadratic equation
+% a_quad = a^2*q*phi - a^2*q*r - a^3*q;
+% b_quad = a^2*q*r - a*mu1*q*r - 2*a^2*mu1*q + 2*a*mu1*q*phi + omega*a*q - omega*a*h - omega*a*mu2 + omega*r*q - omega*r*h - omega*r*mu2;
+% c_quad = a*mu1*q*r - a*mu1^2*q + mu1^2*q*phi - omega*r*q + omega*r*h + omega*r*mu2 + omega*mu1*q - omega*mu1*h - omega*mu1*mu2;
+
+% Dr. Choi's quadratic equation
+a_quad = a*q*(a+r-phi)*(a+r)^2;
+b_quad = (a+r)*(2*a*q*(a+r-phi)*(r-mu1) - r*(omega*(a+r)*(q-h-mu2) - a*q*(a+r) + g*mu1*phi));
+c_quad = a*q*(a+r-phi)*(r-mu1)^2 + r*(omega*(a+r)*(g-h-mu2) - a*q*(a+r) + q*mu1*phi)*(r-mu1) - q*phi*r^2*(mu1+a);
+% quad_quad = [((-b_quad)+((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad); ((-b_quad)-((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad)];
+
+quad_ans = solve(M^2*a_quad + M*b_quad+c_quad == 0, M);

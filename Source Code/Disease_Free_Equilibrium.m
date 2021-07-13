a49094ec21d452cdb4fed66476a44f9b95c5f770
Henry Song <henry.song671@gmail.com>
2021-07-13 11:26:17 +1000

    ENDEMIC

--- Source Code/Disease_Free_Equilibrium.m
+++ Source Code/Disease_Free_Equilibrium.m
@@ -2,7 +2,7 @@ syms C P T M mu1 mu2 q omega sigma beta r h phi g a t
 % g = g(P), a = a(t)
 
 %grazing intensity 'g'
-g = @(P) (omega*P)/beta;
+% g = @(P) (omega*P)/beta;
 
 %sin function of 
 %a = @(t) abs((0.9*(9*sin(pi*t)+1))/(10));
@@ -77,6 +77,7 @@ g = @(P) (omega*P)/beta;
 % C_E = solve(dCdt == 0, C, ReturnConditions = true);
 % %------------------------------------------------------
 
+
 %--------------------------------------------------------------------------
 % Sensitivity Analysis
 % R0 = -(beta*mu1*(a*(mu1/r - 1) - (mu1*phi)/r))/(P*omega*r);
@@ -90,32 +91,32 @@ g = @(P) (omega*P)/beta;
 % end 
 %--------------------------------------------------------------------------
 
-% T_E = (mu1 + a*M)/r;
-% 
-% C_E = 1 - (T+M);
-% C_E = subs(C_E, T, T_E);
-% 
-% P_E = (beta*C*(q-(h+mu2)))/q;
-% P_E = subs(P_E, C, C_E);
-% %testP = (beta*(1-((mu1+a*M)/r) + M)*(q-(h+mu2)))/(q);
-% 
-% M_E = (omega*P)/(beta*(a*C+phi*T))-T;
-% M_E = subs(M_E, P, P_E);
-% M_E = subs(M_E, C, C_E);
-% M_E = subs(M_E, T, T_E);
-% %testM = (omega*(beta*(1-(mu1+a*M)/r + M)*(q-(h+mu2))))/(q*beta*(a*(1-((mu1+a*M)/r + M))) + phi*((mu1+a*M)/r)) - (mu1+a*M)/r;
-% ansM = solve(M_E == 0, M);
-% l = latex(ansM);
-% 
-% % My quadratic equation
-% a_quad = a^2*q*phi - a^2*q*r - a^3*q;
-% b_quad = a^2*q*r - a*mu1*q*r - 2*a^2*mu1*q + 2*a*mu1*q*phi + omega*a*q - omega*a*h - omega*a*mu2 + omega*r*q - omega*r*h - omega*r*mu2;
-% c_quad = a*mu1*q*r - a*mu1^2*q + mu1^2*q*phi - omega*r*q + omega*r*h + omega*r*mu2 + omega*mu1*q - omega*mu1*h - omega*mu1*mu2;
-% 
-% % Dr. Choi's quadratic equation
-% % a_quad = a*q*(a+r-phi)*(a+r)^2;
-% % b_quad = (a+r)*(2*a*q*(a+r-phi)*(r-mu1) - r*(omega*(a+r)*(q-h-mu2) - a*q*(a+r) + g*mu1*phi));
-% % c_quad = a*q*(a+r-phi)*(r-mu1)^2 + r*(omega*(a+r)*(g-h-mu2) - a*q*(a+r) + q*mu1*phi)*(r-mu1) - q*phi*r^2*(mu1+a);
-% % quad_quad = [((-b_quad)+((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad); ((-b_quad)-((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad)];
-% 
-% quad_ans = solve(M^2*a_quad + M*b_quad + c_quad == 0, M);
+T_E = (mu1 + a*M)/r;
+
+C_E = 1 - (T+M);
+C_E = subs(C_E, T, T_E);
+
+P_E = (beta*C*(q-(h+mu2)))/q;
+P_E = subs(P_E, C, C_E);
+%testP = (beta*(1-((mu1+a*M)/r) + M)*(q-(h+mu2)))/(q);
+
+M_E = (omega*P)/(beta*(a*C+phi*T))-T - M;
+M_E = subs(M_E, P, P_E);
+M_E = subs(M_E, C, C_E);
+M_E = subs(M_E, T, T_E);
+%testM = (omega*(beta*(1-(mu1+a*M)/r + M)*(q-(h+mu2))))/(q*beta*(a*(1-((mu1+a*M)/r + M))) + phi*((mu1+a*M)/r)) - (mu1+a*M)/r;
+ansM = solve(M_E == 0, M);
+ansM_latex = latex(ansM);
+
+% My quadratic equation
+a_quad = a^2*q*phi + a*q*phi*r - a^(3)*q - 2*a^(2)*q*r - a*g*r^(2);
+b_quad = a*q*(r^2) + (a^2)*q*r + (mu1)*q*phi*r + 2*a*mu1*q*phi - 2*(a^2)*mu1*q - 2*a*mu1*q*r + a*omega*q*r - a*omega*h*r - a*omega*mu2*r + omega*q*(r^2) - omega*h*(r^2) - omega*mu2*(r^2);
+c_quad = a*mu1*q*r + ((mu1)^2)*q*phi - ((mu1)^2)*q*a + omega*h*(r^2) + omega*mu2*(r^2) + omega*mu1*q*r - omega*q*(r^2) - omega*mu1*h*r - omega*mu1*mu2*r;
+
+% Dr. Choi's quadratic equation
+% a_quad = a*q*(a+r-phi)*(a+r)^2;
+% b_quad = (a+r)*(2*a*q*(a+r-phi)*(r-mu1) - r*(omega*(a+r)*(q-h-mu2) - a*q*(a+r) + g*mu1*phi));
+% c_quad = a*q*(a+r-phi)*(r-mu1)^2 + r*(omega*(a+r)*(g-h-mu2) - a*q*(a+r) + q*mu1*phi)*(r-mu1) - q*phi*r^2*(mu1+a);
+%quad_quad = [((-b_quad)+((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad); ((-b_quad)-((b_quad^2-4*a_quad*c_quad))^0.5)/(2*a_quad)];
+
+quad_ans = solve(M^2*a_quad + M*b_quad + c_quad == 0, M);
